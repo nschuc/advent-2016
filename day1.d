43bf84e83528424a17eafd830198ec75056bca85
Nathan Schucher <nathan.schucher@gmail.com>
2016-12-01 02:12:08 -0500

    part 2

--- day1.d
+++ day1.d
@@ -9,9 +9,18 @@ void main(string[] args) {
     string[] input = stdin.readln().strip().split(", ");
     auto dir = complex(0, 1);
     auto loc = complex(0, 0);
+    bool[string] visited;
+    bool first = false;
     foreach(move; input){
         dir *= move[0] == 'R' ? complex(0, -1) : complex(0, 1);
-        loc += dir * to!int(move[1..$]);
+        for(int i = 0; i < to!int(move[1..$]); i++) {
+            loc += dir;
+            if(!first && to!string(loc) in visited) {
+                writeln("first ", abs(loc.re) + abs(loc.im));
+                first = true;
+            }
+            visited[to!string(loc)] = true;
+        }
     }
-    writeln(abs(loc.re) + abs(loc.im));
+    writeln("final ", abs(loc.re) + abs(loc.im));
 }
